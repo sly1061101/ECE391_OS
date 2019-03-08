5887d00bd6cc4cdcd0a86e6b14f266b2dfeb8530
Wei <zwei13@illinois.edu>
2019-03-07 23:33:45 -0600

    finish init

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -12,32 +12,68 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-    outb(ALL_MASK,MASTER_8259_DATA)   /*mask all of 8258*/
-    outb(ALL_MASK,SLAVE_8259_DATA)
+    outb(ALL_MASK,MASTER_8259_DATA);   /*mask all of 8258*/
+    outb(ALL_MASK,SLAVE_8259_DATA);
 
-    outb(ICW1,MASTER_8259_PORT)
-    outb(ICW1,SLAVE_8259_PORT)
+    outb(ICW1,MASTER_8259_PORT);     /*init all port in cascade mode*/
+    outb(ICW1,SLAVE_8259_PORT);
 
-    outb(ICW2_MASTER ,MASTER_8259_DATA)
-    outb(ICW2_MASTER ,SLAVE_8259_DATA)
+    outb(ICW2_MASTER ,MASTER_8259_DATA);   /*set the pic offset vector*/
+    outb(ICW2_MASTER ,SLAVE_8259_DATA);
 
     
-    outb(ICW3_MASTER ,MASTER_8259_DATA)
-    outb(ICW3_MASTER ,SLAVE_8259_DATA)
+    outb(ICW3_MASTER ,MASTER_8259_DATA);  
+    outb(ICW3_MASTER ,SLAVE_8259_DATA);
 
-    outb(ICW4,MASTER_8259_DATA)   
-    outb(ICW4,SLAVE_8259_DATA)
+    outb(ICW4,MASTER_8259_DATA);   
+    outb(ICW4,SLAVE_8259_DATA);
 
 }
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
+    uint8_t bit_mask = 1;
+    if(irq_num<8) /*for master unmask*/
+    {
+        master_mask = master_mask & (~(bit_mask << irq_num));
+        outb(master_mask,MASTER_8259_DATA);
+    }
+    else /*for slave unmask*/
+    {
+        irq_num = irq_num-8;
+        slave_mask = slave_mask & (~(bit_mask << irq_num));
+        outb(slave_mask,SLAVE_8259_DATA);
+    }
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
+    uint8_t bit_mask = 1;
+    if(irq_num<8) /*for master mask*/
+    {
+        master_mask = master_mask & (bit_mask << irq_num);
+        outb(master_mask,MASTER_8259_DATA);
+    }
+    else /*for slave mask*/
+    {
+        irq_num = irq_num-8;
+        slave_mask = slave_mask & (bit_mask << irq_num);
+        outb(slave_mask,SLAVE_8259_DATA);
+    }
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
+    /*8-15 for slave*/
+    if(irq_num >= 8)
+    {
+        irq_num-=8;
+        outb(EOI|irq_num,SLAVE_8259_PORT);   
+        outb(2|EOI, MASTER_8259_PORT);
+    }
+    /*0-7 for master*/
+    else
+    {
+        outb(EOI|irq_num,MASTER_8259_PORT);
+    }
 }
---
+++ student-distrib/rtc.h
@@ -0,0 +1,11 @@
+#ifndef _RTC_H
+#define _RTC_H
+
+#include "types.h"
+
+
+void rtc_init(void);
+
+extern void rtc_handler(void);
+
+#endif
