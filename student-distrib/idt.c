a6400d313c31009470cce0340414aef34c4ba5bc
Shi <liuyis2@illinois.edu>
2019-03-10 20:18:45 -0500

    Implemented assembly linkage for handlers.

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,19 +1,22 @@
 #include "idt.h"
-#include "x86_desc.h"
 #include "lib.h"
+#include "interrupt.h"
 
 
 #define SYS_CALL 0x80
+
+// Jump table for all interrupt hanlders.
+void (*interrupt_handler[NUM_VEC])();
 
 /* format a macro to handle all different excpetions */
 #define exception(name,message) \
 void name() {  \
     cli();      \
     printf("%s\n", #message);   \
-    while(1);   \
+    sti();   \
 }
 
-exception(exc_de,"0. Divide Error Exception");
+exception(exc_de,"Divide Error Exception");
 exception(exc_db,"Debug Exception");
 exception(exc_nmi,"NMI Interrupt");
 exception(exc_bp,"Breakpoint Exception");
@@ -28,7 +31,7 @@ exception(exc_np,"Segment Not Present");
 exception(exc_ss,"Stack Full Exception");
 exception(exc_gp,"General Protection Exception");
 exception(exc_pf,"Page-Fault Exception");
-// no 15?
+exception(exc_15,"INT 15 Handler!"); // Not exist in Intel manual.
 exception(exc_mf,"x87 FPU Floating_Point Error");
 exception(exc_ac,"Alignment Check Exception");
 exception(exc_mc,"Machine-Check Exception");
@@ -74,28 +77,49 @@ void idt_init(){
         idt[i].seg_selector = KERNEL_CS;
     }
 
+    // Populate interrupt handler array.
+    interrupt_handler[0] = exc_de;
+    interrupt_handler[1] = exc_db;
+    interrupt_handler[2] = exc_nmi;
+    interrupt_handler[3] = exc_bp;
+    interrupt_handler[4] = exc_of;
+    interrupt_handler[5] = exc_br;
+    interrupt_handler[6] = exc_ud;
+    interrupt_handler[7] = exc_nm;
+    interrupt_handler[8] = exc_df;
+    interrupt_handler[9] = exc_cso;
+    interrupt_handler[10] = exc_ts;
+    interrupt_handler[11] = exc_np;
+    interrupt_handler[12] = exc_ss;
+    interrupt_handler[13] = exc_gp;
+    interrupt_handler[14] = exc_pf;
+    interrupt_handler[15] = exc_15;
+    interrupt_handler[16] = exc_mf;
+    interrupt_handler[17] = exc_ac;
+    interrupt_handler[18] = exc_mc;
+    interrupt_handler[19] = exc_xf;
 
 
-    // set up IDT entry 
-    SET_IDT_ENTRY(idt[0],exc_de);
-    SET_IDT_ENTRY(idt[1],exc_db);
-    SET_IDT_ENTRY(idt[2],exc_nmi);
-    SET_IDT_ENTRY(idt[3],exc_bp);
-    SET_IDT_ENTRY(idt[4],exc_of);
-    SET_IDT_ENTRY(idt[5],exc_br);
-    SET_IDT_ENTRY(idt[6],exc_ud);
-    SET_IDT_ENTRY(idt[7],exc_nm);
-    SET_IDT_ENTRY(idt[8],exc_df);
-    SET_IDT_ENTRY(idt[9],exc_cso);
-    SET_IDT_ENTRY(idt[10],exc_ts);
-    SET_IDT_ENTRY(idt[11],exc_np);
-    SET_IDT_ENTRY(idt[12],exc_ss);
-    SET_IDT_ENTRY(idt[13],exc_gp);
-    SET_IDT_ENTRY(idt[14],exc_pf);
-    SET_IDT_ENTRY(idt[16],exc_mf);
-    SET_IDT_ENTRY(idt[17],exc_ac);
-    SET_IDT_ENTRY(idt[18],exc_mc);
-    SET_IDT_ENTRY(idt[19],exc_xf);
-
+    // set up IDT entry
+    SET_IDT_ENTRY(idt[0],ir_linkage_0);
+    SET_IDT_ENTRY(idt[1],ir_linkage_1);
+    SET_IDT_ENTRY(idt[2],ir_linkage_2);
+    SET_IDT_ENTRY(idt[3],ir_linkage_3);
+    SET_IDT_ENTRY(idt[4],ir_linkage_4);
+    SET_IDT_ENTRY(idt[5],ir_linkage_5);
+    SET_IDT_ENTRY(idt[6],ir_linkage_6);
+    SET_IDT_ENTRY(idt[7],ir_linkage_7);
+    SET_IDT_ENTRY(idt[8],ir_linkage_8);
+    SET_IDT_ENTRY(idt[9],ir_linkage_9);
+    SET_IDT_ENTRY(idt[10],ir_linkage_10);
+    SET_IDT_ENTRY(idt[11],ir_linkage_11);
+    SET_IDT_ENTRY(idt[12],ir_linkage_12);
+    SET_IDT_ENTRY(idt[13],ir_linkage_13);
+    SET_IDT_ENTRY(idt[14],ir_linkage_14);
+    SET_IDT_ENTRY(idt[15],ir_linkage_15);
+    SET_IDT_ENTRY(idt[16],ir_linkage_16);
+    SET_IDT_ENTRY(idt[17],ir_linkage_17);
+    SET_IDT_ENTRY(idt[18],ir_linkage_18);
+    SET_IDT_ENTRY(idt[19],ir_linkage_19);
 
 }
---
+++ student-distrib/interrupt.h
@@ -0,0 +1,29 @@
+#ifndef _INTERRUPT_H_
+#define _INTERRUPT_H_
+
+#ifndef ASM
+
+extern void ir_linkage_0();
+extern void ir_linkage_1();
+extern void ir_linkage_2();
+extern void ir_linkage_3();
+extern void ir_linkage_4();
+extern void ir_linkage_5();
+extern void ir_linkage_6();
+extern void ir_linkage_7();
+extern void ir_linkage_8();
+extern void ir_linkage_9();
+extern void ir_linkage_10();
+extern void ir_linkage_11();
+extern void ir_linkage_12();
+extern void ir_linkage_13();
+extern void ir_linkage_14();
+extern void ir_linkage_15();
+extern void ir_linkage_16();
+extern void ir_linkage_17();
+extern void ir_linkage_18();
+extern void ir_linkage_19();
+
+#endif
+
+#endif
