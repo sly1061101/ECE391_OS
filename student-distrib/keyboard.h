c384ab3d6bef81ed02756e153813998579e4b7f6
sly1061101 <sly1061101@gmail.com>
2019-03-11 13:23:28 -0500

    Keyboard init/handler almost done

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -3,9 +3,11 @@
 
 #include "i8259.h"
 #include "tests.h"
-#include "lib.h" //maybe
+#include "lib.h"
 
 #define KEYBOARD_IRQ 0x01
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
 
 /* Initialize the keyboard */
 extern void keyboard_init();
