19e71b8c91818b925919f897bceecf05e62aad07
Zhou <tzhou26@illinois.edu>
2019-03-15 02:08:24 -0500

    Added additional cases, debug needed

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -4,22 +4,25 @@
 #include "keyboard.h"
 #include "idt.h"
 
-
 #define LOW_BIT_OFFSER 0x01
 
+int rtc_test = 0;
+
+int caps_flag = 0;
+int shift_flag = 0;
+int alt_flag = 0;
+int ctrl_flag = 0;
 /* keyboard_init
  * Initialized the keyboard
  * Inputs: None
  * Outputs: None
  * Side Effects: Enable irq 1
  */
-void keyboard_init(){
-    interrupt_handler[KEYBOARD_IR_VEC] = keyboard_handler;
-    enable_irq(KEYBOARD_IRQ);
-
+void keyboard_init()
+{
+  interrupt_handler[KEYBOARD_IR_VEC] = keyboard_handler;
+  enable_irq(KEYBOARD_IRQ);
 }
-
-int rtc_test = 0;
 
 // reference https://github.com/arjun024/mkeykernel
 /* keyboard_handler
@@ -28,34 +31,42 @@ int rtc_test = 0;
  * Outputs: None
  * Side Effects: none
  */
-void keyboard_handler(){
+void keyboard_handler()
+{
 
-    unsigned char status;
-    char keycode;
-    unsigned char keycode_processed;
+  unsigned char status;
+  char keycode;
+  unsigned char keycode_processed;
 
-    status = inb(KEYBOARD_STATUS_PORT);
+  status = inb(KEYBOARD_STATUS_PORT);
 
   /* Lowest bit of status check empty */
-      if (status & LOW_BIT_OFFSER) {
-		keycode = inb(KEYBOARD_DATA_PORT);
-    keycode_processed = char_converter(keycode);
-		  if(keycode >= 0){
-        
-        //printf("%c",keyboard_map[keycode]);
-        printf("%c",keycode_processed);
+  if (status & LOW_BIT_OFFSER)
+  {
+    keycode = inb(KEYBOARD_DATA_PORT);
+    keycode_processed = char_converter((unsigned char)keycode);
 
-        if(keyboard_map[(unsigned char)keycode] == 'r')
-          rtc_test = 1;
-        else
-          rtc_test = 0;
+    if (keycode >= 0)
+    {
 
-
+      if (keycode_processed == 'l' || keycode_processed == 'L')
+      {
+        clear(); // clear screen
+        send_eoi(KEYBOARD_IRQ);
+        return;
       }
 
+      //printf("%c",keyboard_map[keycode]);
+      printf("%c", keycode_processed);
+
+      if (keyboard_map[(unsigned char)keycode] == 'r')
+        rtc_test = 1;
+      else
+        rtc_test = 0;
+    }
+
     send_eoi(KEYBOARD_IRQ);
-    
-}
+  }
 }
 
 /* char_converter
@@ -64,12 +75,60 @@ void keyboard_handler(){
  * Outputs: converted character 
  * Side Effects:s none
  */
-char char_converter(char input){
+char char_converter(unsigned char input)
+{
 
-    // char input_test = keyboard_map[input];
-    // if(input_test==TAB){
-    //   return keyboard_map_caps[input_test];
-    // }
-     return keyboard_map[input];
-    
-} 
+  switch ((unsigned)input)
+  {
+  case CAPS_LOCK:
+    caps_flag = PRESSED - caps_flag;
+    break;
+
+  case LEFT_SHIFT:
+  case RIGHT_SHIFT:
+    shift_flag = PRESSED;
+    break;
+
+  // case LEFT_SHIFT + HIGH_ORDER_BIT_MASK:
+  case LEFT_SHIFT_R:
+ // case RIGHT_SHIFT + HIGH_ORDER_BIT_MASK:
+  case RIGHT_SHIFT_R:
+    shift_flag = RELEASED;
+    break;
+
+  case CTRL:
+    ctrl_flag = PRESSED;
+    break;
+
+  case CTRL + HIGH_ORDER_BIT_MASK:
+    ctrl_flag = RELEASED;
+    break;
+
+  case ALT:
+    alt_flag = PRESSED;
+    break;
+
+  case ALT + HIGH_ORDER_BIT_MASK:
+    alt_flag = RELEASED;
+    break;
+
+  default:
+    break;
+  }
+
+  if (caps_flag)
+  {
+    if(shift_flag)
+    {
+      return keyboard_map_shift[(unsigned char)input];
+    }
+    return keyboard_map_caps[(unsigned char)input];
+  }
+
+  if (shift_flag)
+  {
+    return keyboard_map_shift[(unsigned char)input];
+  }
+
+  return keyboard_map[(unsigned char)input];
+}
