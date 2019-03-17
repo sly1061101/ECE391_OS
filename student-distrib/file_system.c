f67f3abaa431324fdaff21071c0a64a7830812ab
Shi <liuyis2@illinois.edu>
2019-03-17 14:15:29 -0500

    Fixed struct definitions and corresponding codes.

--- student-distrib/file_system.c
+++ student-distrib/file_system.c
@@ -4,19 +4,20 @@
 
 static uint32_t file_system_base_address = NULL;
 
+typedef struct dentry {
+    uint8_t file_name[32];
+    uint32_t file_type;
+    uint32_t inode_idx;
+    uint8_t reserved[24];
+} dentry_t;
+
 typedef struct boot_block {
     uint32_t num_dentry;
     uint32_t num_inode;
     uint32_t num_data_block;
     uint8_t reserved[52];
+    dentry_t dentry[63];
 } boot_block_t;
-
-typedef struct dentry {
-    uint8_t file_name[32];
-    uint32_t file_type;
-    uint32_t inode_num;
-    uint8_t reserved[24];
-} dentry_t;
 
 static int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
 static int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
@@ -30,7 +31,7 @@ int32_t read_dentry_by_name(const uint8_t *fname, dentry_t *dentry) {
 
     int i;
     for(i = 0; i < boot_block->num_dentry; ++i) {
-        source = (dentry_t *)((char *)file_system_base_address + sizeof(boot_block_t) + i * sizeof(dentry_t));
+        source = (dentry_t *)((char *)file_system_base_address + 64 + i * sizeof(dentry_t));
         if(strncmp((uint8_t *)fname, (uint8_t *)source->file_name, 32) == 0) {
             read_dentry_by_index(i, dentry);
             return 0;
@@ -48,7 +49,7 @@ int32_t read_dentry_by_index(uint32_t index, dentry_t *dentry) {
     if(index >= boot_block->num_dentry)
         return -1;
 
-    dentry_t *source = (dentry_t *)((char *)file_system_base_address + sizeof(boot_block_t) + index * sizeof(dentry_t));
+    dentry_t *source = (dentry_t *)((char *)file_system_base_address + 64 + index * sizeof(dentry_t));
     memcpy((void *)dentry, (void *)source, sizeof(dentry_t));
 
     return 0;
