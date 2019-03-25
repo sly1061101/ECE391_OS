d6dc381a165ec63e41457c404626bc023a5e0788
Shi <liuyis2@illinois.edu>
2019-03-24 21:44:17 -0500

    Modifed scope of functions in file system.

--- student-distrib/file_system.h
+++ student-distrib/file_system.h
@@ -12,6 +12,38 @@
 #define DIRECTORY_FILE 1
 #define REGULAR_FILE 2
 
+// Structs corresponding to file system specification.
+typedef struct dentry {
+    uint8_t file_name[32];
+    uint32_t file_type;
+    uint32_t inode_idx;
+    uint8_t reserved[24];
+} dentry_t;
+
+typedef struct boot_block {
+    uint32_t num_dentry;
+    uint32_t num_inode;
+    uint32_t num_data_block;
+    uint8_t reserved[52];
+    dentry_t dentry[63];
+} boot_block_t;
+
+typedef struct inode {
+    uint32_t length;
+    uint32_t date_block_idx[1023];
+} inode_t;
+
+typedef struct data_block {
+    uint8_t data[4096];
+} data_block_t;
+
+// Three helper routines that actually interacts with file system.
+extern int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
+extern int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
+extern int32_t read_data (uint32_t inode, uint32_t offset, 
+                                uint8_t* buf, uint32_t length);
+
+// File system interfaces.
 extern int file_system_init(uint32_t base_address);
 
 extern int32_t file_open(const uint8_t *filename);
