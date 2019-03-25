552bcb2de73efa139eaf709c5c55811922aa088c
Wei <zwei13@illinois.edu>
2019-03-24 20:58:42 -0500

    rtc add write read

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -15,7 +15,7 @@
 #define RATE_OFFSET 0xF0
 #define IRQ8 8
 
-
+volatile int is_interrupt;
 //void rtc_handler(void);
 
 /*reference from https://wiki.osdev.org/RTC */
@@ -64,42 +64,156 @@ void rtc_init(void)
 void rtc_handler(void)
 {
     cli();
+    is_interrupt = 1;
     outb(RTC_REG_C,RTC_REG_PORT);	// select register C
     inb(RTC_REG_DATA);		// just throw away contents
     send_eoi(IRQ8);
     sti();
 }
 
-void rtc_set_freq(int freq)
-{
+// void rtc_set_freq(int freq)
+// {
 
+//     cli();
+//     outb(REG_SET|REG_A, RTC_REG_PORT);
+//     unsigned char prev = inb(RTC_REG_DATA);
+//     outb(REG_SET|REG_A, RTC_REG_PORT);
+//     outb((prev & 0xF0)|rate, RTC_REG_DATA);
+//     sti();
+
+
+
+// }
+
+// int32_t rtc_open()
+// {
+//     return 0;
+// }
+
+// int32_t rtc_close()
+// {
+//     return 0;
+// }
+
+// int32_t rtc_read()
+// {
+//     return 0;
+// }
+
+// int32_t rtc_write()
+// {
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+int32_t rtc_open (const uint8_t* filename){
+    //change the rate:
+    unsigned n_rate =2;//rate change to 2 herz?
     cli();
-    outb(REG_SET|REG_A, RTC_REG_PORT);
-    unsigned char prev = inb(RTC_REG_DATA);
-    outb(REG_SET|REG_A, RTC_REG_PORT);
-    outb((prev & 0xF0)|rate, RTC_REG_DATA);
+    outb(RTC_REG_A,RTC_REG_PORT);
+    char prev=inb(RTC_REG_DATA);
+    outb(RTC_REG_A,RTC_REG_PORT);
+    outb((prev&0xF0)|n_rate,RTC_REG_DATA);
+    sti();
+	//enable irq
+	enable_irq(IRQ8);
+	
+	return 0;
+
+}
+
+int32_t rtc_close(int32_t fd){
+    
+    return 0;
+}
+int32_t rtc_read(int32_t fd,void*buf,int32_t nbytes){
+	
+	while(!is_interrupt);
+    is_interrupt = 0;
+	return 0;
+}
+
+
+
+
+int32_t write(int32_t fd,const void*buf,int32_t nbytes){
+	 
+	cli();
+    if(buf == NULL || nbytes != 4)
+    {
+        sti();
+        return -1;
+    }
+    int32_t freq;
+    int32_t * buffer = (int32_t*) buf;
+    freq = *buffer;
+    set_freq(freq);
+    if(freq == -1)
+    {
+        sti();
+        return -1;
+    }
+
+    outb(RTC_REG_A,RTC_REG_PORT);
+    char prev=inb(RTC_REG_DATA);
+    outb(RTC_REG_A,RTC_REG_PORT);
+    outb((prev&0xF0)|freq,RTC_REG_DATA);
     sti();
 
-
-
+    return nbytes;
 }
 
-int32_t rtc_open()
-{
-    return 0;
+
+void set_freq(int32_t freq){
+    switch(freq){
+      case 1024:
+                freq = 0x6;
+                break;
+      case 512:
+                freq = 0x7;
+                break;
+      case 256:
+                freq = 0x8;
+                break;
+      case 128:
+                freq = 0x9;
+                break;
+      case 64:
+                freq = 0xA;
+                break;
+      case 32:
+                freq = 0xB;
+                break;
+      case 16:
+                freq = 0xC;
+                break;
+      case 8:
+                freq = 0xD;
+                break;
+      case 4:
+                freq = 0xE;
+                break;
+      case 2:
+                freq = 0xF;
+                break;
+      default:
+                freq = -1;
+                break;
+    }
 }
 
-int32_t rtc_close()
-{
-    return 0;
-}
 
-int32_t rtc_read()
-{
-    return 0;
-}
 
-int32_t rtc_write()
-{
-    return 0;
-}
+
+
+
+
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -5,9 +5,9 @@
 
 void rtc_handler(void); // rtc handler
 void rtc_init(void); //rtc initialization
-void rtc_set_freq(int freq); // rtc set freq
-int32_t rtc_open();
-int32_t rtc_close();
-int32_t rtc_read();
-int32_t rtc_write();
+void set_freq(int32_t freq); // rtc set freq
+int32_t rtc_open(const uint8_t* filename);
+int32_t rtc_close(int32_t fd);
+int32_t rtc_read(int32_t fd,void*buf,int32_t nbytes);
+int32_t write(int32_t fd,const void*buf,int32_t nbytes);
 #endif
