dc87fb4a918bf6d4ee2c32037f7b68983a9f261d
Wei <zwei13@illinois.edu>
2019-03-24 21:05:01 -0500

    change name for rtc_write

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -100,7 +100,7 @@ int32_t rtc_read(int32_t fd,void*buf,int32_t nbytes){
 }
 
 
-int32_t write(int32_t fd,const void*buf,int32_t nbytes){
+int32_t rtc_write(int32_t fd,const void*buf,int32_t nbytes){
 	 
 	cli();
     if(buf == NULL || nbytes != 4)
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -9,5 +9,5 @@ void set_freq(int32_t freq); // rtc set freq
 int32_t rtc_open(const uint8_t* filename);
 int32_t rtc_close(int32_t fd);
 int32_t rtc_read(int32_t fd,void*buf,int32_t nbytes);
-int32_t write(int32_t fd,const void*buf,int32_t nbytes);
+int32_t rtc_write(int32_t fd,const void*buf,int32_t nbytes);
 #endif
