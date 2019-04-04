412fb1fd1db350823e1519f7736f0247853272dc
Shi <liuyis2@illinois.edu>
2019-04-03 21:59:41 -0500

    Added codes to parse and validate executable name.

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -1,4 +1,6 @@
 #include "syscall.h"
+#include "file_system.h"
+#include "keyboard.h"
 
 uint32_t syscall_jump_table[11] =   {   0,
                                         (uint32_t)syscall_halt, (uint32_t)syscall_execute, (uint32_t)syscall_read,
@@ -12,6 +14,29 @@ int32_t syscall_halt (uint8_t status) {
 }
 
 int32_t syscall_execute (const uint8_t* command) {
+    if(command == NULL)
+        return -1;
+    int i;
+
+    // Parse the executable name.
+    // TODO: Parse remaining arguments.
+    uint8_t filename[FILE_NAME_MAX_LENGTH + 1];
+    for(i = 0; i < strlen(command) + 1; ++i) {
+        if(command[i] == ' ' || command[i] == '\0') {
+            memcpy(filename, command, i);
+            filename[i] = 0;
+            break;
+        }
+        
+        if(i >= FILE_NAME_MAX_LENGTH)
+            return -1;
+    }
+
+    if(!check_executable(filename))
+        return -1;
+    
+    
+
     return -1;
 }
 
