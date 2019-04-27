06982b0d3f24b393f137673395725496b0dc5aa7
Shi <liuyis2@illinois.edu>
2019-04-27 16:27:42 -0500

    Move keyboard scancode into a seperate header file.

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -11,7 +11,6 @@
 #define KEYBOARD_STATUS_PORT 0x64
 #define LOW_BIT_OFFSER 0x01
 
-#define MAP_SIZE 128
 #define KEYBOARD_BUFFER_CAPACITY 128
 
 #define LEFT_SHIFT 0x2A
@@ -35,18 +34,6 @@
 
 /* The scancode for key release (`break') is obtained from it by setting the high order bit */
 #define HIGH_ORDER_BIT_MASK 0x80
-
-/* US Keyboard Layout for general cases*/
-extern unsigned char keyboard_map[MAP_SIZE];
-
-/* US Keyboard Layout for CAPSLOCK cases*/
-extern unsigned char keyboard_map_caps[MAP_SIZE];
-
-/* US Keyboard Layout for SHIFT lower cases*/
-extern unsigned char keyboard_map_shift_lower[MAP_SIZE];
-
-/* US Keyboard Layout for SHIFT upper cases*/
-extern unsigned char keyboard_map_shift_upper[MAP_SIZE];
 
 /* Initialize the keyboard */
 extern void keyboard_init();
